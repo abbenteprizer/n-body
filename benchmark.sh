554aa0a0701d4e4630d7201e049b047532628c90
abbenteprizer <abbenteprizer@gmail.com>
2019-03-14 14:08:33 +0100

    corrected names

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-g++ -o pnbody pnbody.cpp -fopenmp
-gnuplot imageScriptParallel.p
+
+./pnbody 100 1 1000 >  bench.dat
+./pnbody 100 2 1000 >>  bench.dat
+./pnbody 100 3 1000 >>  bench.dat
+./pnbody 100 4 1000 >>  bench.dat
+./pnbody 100 5 1000 >>  bench.dat
+./pnbody 100 6 1000 >>  bench.dat
+./pnbody 100 7 1000 >>  bench.dat
+./pnbody 100 8 1000 >>  bench.dat
+./pnbody 100 9 1000 >>  bench.dat
+./pnbody 100 10 1000 >>  bench.dat
+./pnbody 100 11 1000 >>  bench.dat
+./pnbody 100 12 1000 >>  bench.dat
+./pnbody 100 13 1000 >>  bench.dat
+./pnbody 100 14 1000 >>  bench.dat
+./pnbody 100 15 1000 >>  bench.dat
+./pnbody 100 16 1000 >>  bench.dat
