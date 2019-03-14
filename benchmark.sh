4c09a5c073a1b9fad208e0ca2f199acd8e86cc17
abbenteprizer <abbenteprizer@gmail.com>
2019-03-14 16:04:03 +0100

    corrected bench script

--- benchmark.sh
+++ benchmark.sh
@@ -1,54 +1,59 @@
 #!/bin/bash
 
-./pnbody 120 1000 1  >  bench120.dat
-./pnbody 120 1000 2  >>  bench120.dat
-./pnbody 120 1000 3  >>  bench120.dat
-./pnbody 120 1000 4  >>  bench120.dat
-./pnbody 120 1000 5  >>  bench120.dat
-./pnbody 120 1000 6  >>  bench120.dat
-./pnbody 120 1000 7  >>  bench120.dat
-./pnbody 120 1000 8  >>  bench120.dat
-./pnbody 120 1000 9  >>  bench120.dat
-./pnbody 120 1000 10  >>  bench120.dat
-./pnbody 120 1000 11  >>  bench120.dat
-./pnbody 120 1000 12  >>  bench120.dat
-./pnbody 120 1000 13  >>  bench120.dat
-./pnbody 120 1000 14  >>  bench120.dat
-./pnbody 120 1000 15  >>  bench120.dat
-./pnbody 120 1000 16  >>  bench120.dat
+./pnbody 120 1000 1  >  data/bench120.dat
+./pnbody 120 1000 2  >>  data/bench120.dat
+./pnbody 120 1000 3  >>  data/bench120.dat
+./pnbody 120 1000 4  >>  data/bench120.dat
+./pnbody 120 1000 5  >>  data/bench120.dat
+./pnbody 120 1000 6  >>  data/bench120.dat
+./pnbody 120 1000 7  >>  data/bench120.dat
+./pnbody 120 1000 8  >>  data/bench120.dat
+./pnbody 120 1000 9  >>  data/bench120.dat
+./pnbody 120 1000 10  >>  data/bench120.dat
+./pnbody 120 1000 11  >>  data/bench120.dat
+./pnbody 120 1000 12  >>  data/bench120.dat
+./pnbody 120 1000 13  >>  data/bench120.dat
+./pnbody 120 1000 14  >>  data/bench120.dat
+./pnbody 120 1000 15  >>  data/bench120.dat
+./pnbody 120 1000 16  >>  data/bench120.dat
+
+./snbody 120 1000 > data/sbench120.dat
+
+./pnbody 180 1000 1  >  data/bench180.dat
+./pnbody 180 1000 2  >>  data/bench180.dat
+./pnbody 180 1000 3  >>  data/bench180.dat
+./pnbody 180 1000 4  >>  data/bench180.dat
+./pnbody 180 1000 5  >>  data/bench180.dat
+./pnbody 180 1000 6  >>  data/bench180.dat
+./pnbody 180 1000 7  >>  data/bench180.dat
+./pnbody 180 1000 8  >>  data/bench180.dat
+./pnbody 180 1000 9  >>  data/bench180.dat
+./pnbody 180 1000 10  >>  data/bench180.dat
+./pnbody 180 1000 11  >>  data/bench180.dat
+./pnbody 180 1000 12  >>  data/bench180.dat
+./pnbody 180 1000 13  >>  data/bench180.dat
+./pnbody 180 1000 14  >>  data/bench180.dat
+./pnbody 180 1000 15  >>  data/bench180.dat
+./pnbody 180 1000 16  >>  data/bench180.dat
+
+./snbody 180 1000 > data/sbench180.dat
 
 
-./pnbody 180 1000 1  >  bench180.dat
-./pnbody 180 1000 2  >>  bench180.dat
-./pnbody 180 1000 3  >>  bench180.dat
-./pnbody 180 1000 4  >>  bench180.dat
-./pnbody 180 1000 5  >>  bench180.dat
-./pnbody 180 1000 6  >>  bench180.dat
-./pnbody 180 1000 7  >>  bench180.dat
-./pnbody 180 1000 8  >>  bench180.dat
-./pnbody 180 1000 9  >>  bench180.dat
-./pnbody 180 1000 10  >>  bench180.dat
-./pnbody 180 1000 11  >>  bench180.dat
-./pnbody 180 1000 12  >>  bench180.dat
-./pnbody 180 1000 13  >>  bench180.dat
-./pnbody 180 1000 14  >>  bench180.dat
-./pnbody 180 1000 15  >>  bench180.dat
-./pnbody 180 1000 16  >>  bench180.dat
+./pnbody 240 1000 1  >  data/bench240.dat
+./pnbody 240 1000 2  >>  data/bench240.dat
+./pnbody 240 1000 3  >>  data/bench240.dat
+./pnbody 240 1000 4  >>  data/bench240.dat
+./pnbody 240 1000 5  >>  data/bench240.dat
+./pnbody 240 1000 6  >>  data/bench240.dat
+./pnbody 240 1000 7  >>  data/bench240.dat
+./pnbody 240 1000 8  >>  data/bench240.dat
+./pnbody 240 1000 9  >>  data/bench240.dat
+./pnbody 240 1000 10  >>  data/bench240.dat
+./pnbody 240 1000 11  >>  data/bench240.dat
+./pnbody 240 1000 12  >>  data/bench240.dat
+./pnbody 240 1000 13  >>  data/bench240.dat
+./pnbody 240 1000 14  >>  data/bench240.dat
+./pnbody 240 1000 15  >>  data/bench240.dat
+./pnbody 240 1000 16  >>  data/bench240.dat
 
-
-./pnbody 240 1000 1  >  bench240.dat
-./pnbody 240 1000 2  >>  bench240.dat
-./pnbody 240 1000 3  >>  bench240.dat
-./pnbody 240 1000 4  >>  bench240.dat
-./pnbody 240 1000 5  >>  bench240.dat
-./pnbody 240 1000 6  >>  bench240.dat
-./pnbody 240 1000 7  >>  bench240.dat
-./pnbody 240 1000 8  >>  bench240.dat
-./pnbody 240 1000 9  >>  bench240.dat
-./pnbody 240 1000 10  >>  bench240.dat
-./pnbody 240 1000 11  >>  bench240.dat
-./pnbody 240 1000 12  >>  bench240.dat
-./pnbody 240 1000 13  >>  bench240.dat
-./pnbody 240 1000 14  >>  bench240.dat
-./pnbody 240 1000 15  >>  bench240.dat
-./pnbody 240 1000 16  >>  bench240.dat
+./snbody 240 1000 > data/sbench240.dat
